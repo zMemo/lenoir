2a3e85f0de18a3feea4a0a36802855a276a35b9b
memoyinko <memoyinko@gmail.com>
2024-10-25 21:24:03 -0300

    bd

--- lenoir/screens/bd/admin.sql
+++
@@ -1,7 +0,0 @@
-/* Entrando a la sesión "lenoir" */
-SHOW CREATE TABLE `lenoir`.`adminlog`;
-SELECT * FROM `lenoir`.`adminlog` LIMIT 1000;
-INSERT INTO `lenoir`.`adminlog` (`idadmin`) VALUES (1);
-SELECT `idadmin`, `user`, `password` FROM `lenoir`.`adminlog` WHERE  `idadmin`=1;
-UPDATE `lenoir`.`adminlog` SET `user`='memo', `password`='lenoirmemo' WHERE  `idadmin`=1;
-SELECT `idadmin`, `user`, `password` FROM `lenoir`.`adminlog` WHERE  `idadmin`=1;
---
+++ lenoir/screens/bd/lenoir (1).sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 15-10-2024 a las 15:50:02
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `lenoir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrega`
+--
+
+CREATE TABLE `entrega` (
+  `id` int(11) NOT NULL,
+  `id_pedido_producto` int(11) NOT NULL,
+  `id_oferta` int(11) NOT NULL,
+  `fechahora_entrega` date NOT NULL,
+  `estado` enum('Cancelado','En proceso','En camino','Entregado') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `oferta`
+--
+
+CREATE TABLE `oferta` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `fechahora` date NOT NULL,
+  `estado` enum('Inactivo','Activo') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `oferta_detalle`
+--
+
+CREATE TABLE `oferta_detalle` (
+  `id` int(11) NOT NULL,
+  `id_oferta` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_total` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_detalle`
+--
+
+CREATE TABLE `pedido_detalle` (
+  `id` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_oferta`
+--
+
+CREATE TABLE `pedido_oferta` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fechahora_pedido` date NOT NULL,
+  `precio_total` float NOT NULL,
+  `id_oferta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido_producto`
+--
+
+CREATE TABLE `pedido_producto` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fechahora_pedido` date NOT NULL,
+  `preciototal` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `idProducto` int(11) NOT NULL,
+  `producto` varchar(30) NOT NULL,
+  `categoria` enum('Vinos','Cervezas','Sin alcohol','Destilados','Extras') NOT NULL,
+  `marca` varchar(30) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `descprod` text NOT NULL,
+  `img` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `mail` varchar(45) NOT NULL,
+  `pass` int(30) NOT NULL,
+  `telefono` int(15) NOT NULL,
+  `nacimiento` date NOT NULL,
+  `tipo_usuario` enum('Cliente','Admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `entrega`
+--
+ALTER TABLE `entrega`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `oferta`
+--
+ALTER TABLE `oferta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `oferta_detalle`
+--
+ALTER TABLE `oferta_detalle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedido_detalle`
+--
+ALTER TABLE `pedido_detalle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedido_oferta`
+--
+ALTER TABLE `pedido_oferta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pedido_producto`
+--
+ALTER TABLE `pedido_producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`),
+  ADD UNIQUE KEY `mail` (`mail`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `entrega`
+--
+ALTER TABLE `entrega`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `oferta`
+--
+ALTER TABLE `oferta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `oferta_detalle`
+--
+ALTER TABLE `oferta_detalle`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido_detalle`
+--
+ALTER TABLE `pedido_detalle`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido_oferta`
+--
+ALTER TABLE `pedido_oferta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido_producto`
+--
+ALTER TABLE `pedido_producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
