83acc846bc2e269ee049cd73f2b7cb2ef2968680
memoyinko <memoyinko@gmail.com>
2025-02-22 21:52:06 -0300

    update code structure

--- _jinja/upload/route.py
+++ _jinja/upload/route.py
@@ -4,3 +4,21 @@ from werkzeug.utils import secure_filename
 from appConfig import config
 from uuid import uuid4 
 import os
+
+def route(app):
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+    
+    @app.route('/recibir_datos', methods=['POST', 'GET'])
+    def recibir_datos():
+        if request.method == 'POST':
+            nombre = request.form['nombre']
+            edad = request.form['edad']
+            return render_template('datos.html', nombre=nombre, edad=edad)
+        else:
+            return redirect(url_for('index'))
